66ae93e920160a92ed55acc200d7b6d682e0b578
“Sofia <Sofya_Shumova@epam.com>
2021-03-05 01:43:34 +0300

    creating spinner

---
+++ client/src/components/spinner/Spinner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className={style.loadingio_spinner_ellipsis_bqgom1pmt5s}>
+        <div className={style.ldio_szcmqfjmhys}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
