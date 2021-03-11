12752446b4a44d2d2580c16cc2e3e50e318b8c8a
“Sofia <Sofya_Shumova@epam.com>
2021-03-12 01:04:46 +0300

    create context

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { UserContext } from '../context';
 import { navigation } from '../router/navigation';
 import Header from '../header/Header';
 
-const isAuth = false;
-const user = {
-  isExpert: false,
-  isAdmin: false,
-};
-
 const App = () => {
-  const { Routes, links } = navigation(isAuth, user);
+  const [user, setUser] = useState(null);
+  const { Routes, links } = navigation(user);
+
   return (
-    <Router>
-      <div className="wrapper">
-        <Header>{links}</Header>
-        <Routes />
-      </div>
-    </Router>
+    <UserContext.Provider value={user}>
+      <Router>
+        <div className="wrapper">
+          <Header>{links}</Header>
+          <Routes />
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 };
 
