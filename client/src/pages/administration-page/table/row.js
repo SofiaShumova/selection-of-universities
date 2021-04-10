ca2051eac409aca8fa24920f21f919346f85c186
“Sofia <Sofya_Shumova@epam.com>
2021-04-10 19:23:21 +0300

    create table component

---
+++ client/src/pages/administration-page/table/row.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { v4 as uuid } from 'uuid';
+import { Value } from './value';
+
+export const Row = ({ schema, item }) => {
+  return (
+    <tr>
+      {Object.entries(schema).map(([key, value]) => {
+        return (
+          <td key={uuid()}>
+            <Value
+              component={value.element ?? value}
+              prop={value.key ?? key}
+              item={item}
+            />
+          </td>
+        );
+      })}
+    </tr>
+  );
+};
