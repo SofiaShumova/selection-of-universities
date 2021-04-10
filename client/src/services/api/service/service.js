c3ae588799ca1344d8ef5469d35d3fc6eaddbde6
“Sofia <Sofya_Shumova@epam.com>
2021-04-10 19:22:31 +0300

    create server service

---
+++ client/src/services/api/service/service.js
@@ -0,0 +1,16 @@
+export default class Service {
+  _apiBase = 'http://localhost:5000/api';
+
+  getResourse = async (url) => {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} received ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllUniversity = async () => {
+    return await this.getResourse(`/university/`);
+  };
+}
