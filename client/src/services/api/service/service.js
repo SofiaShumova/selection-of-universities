47f056b375a534762ee65ec1e9bb398873fe6120
“Sofia <Sofya_Shumova@epam.com>
2021-04-22 13:27:24 +0300

    update service for university

--- client/src/services/api/service/service.js
+++ client/src/services/api/service/service.js
@@ -10,9 +10,70 @@ export default class Service {
     return await res.json();
   };
 
+  addResourse = async (url, data) => {
+    const res = await fetch(`${this._apiBase}${url}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} received ${res.status}`);
+    }
+
+    return await res.ok;
+  };
+
+  updateResourse = async (url, data) => {
+    const res = await fetch(`${this._apiBase}${url}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} received ${res.status}`);
+    }
+
+    return await res.ok;
+  };
+
+  deleteResourse = async (url) => {
+    const res = await fetch(`${this._apiBase}${url}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} received ${res.status}`);
+    }
+
+    return await res.ok;
+  };
+
   getAllUniversity = async () => {
     return await this.getResourse(`/university/`);
   };
+  addUniversity = async (university) => {
+    return await this.addResourse('/university/', university);
+  };
+  updateUniversity = async (university) => {
+    return await this.updateResourse(
+      `/university/${university._id}`,
+      university
+    );
+  };
+
+  removeUniversity = async (university) => {
+    return await this.deleteResourse(`/university/${university._id}`);
+  };
+
   getAllCity = async () => {
     return await this.getResourse(`/city/`);
   };
